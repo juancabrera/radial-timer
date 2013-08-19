66ad918434f481257a3739936b1f3c91368b533d
Juan Cabrera <juan.acc@gmail.com>
2013-08-19 14:12:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+radialTimer()
+=============
+A Javascript method that creates a CSS radial timer/countdown with progress bar.
---
+++ public_html/js/radialtimer.js
@@ -0,0 +1,42 @@
+function radialTimer() {
+	var self = this;
+
+	this.seconds = 0;
+	this.count = 0;
+	this.degrees = 0;
+	this.interval = null;
+	this.number = $(".n");
+	this.slice = $(".slice");
+	this.pie = $(".pie");
+	this.pieRight = $(".pie.r");
+	this.pieLeft = $(".pie.l");
+	this.quarter = $(".q");
+
+	this.init = function(s) {
+		self.seconds = s;
+		self.interval = window.setInterval(function () {
+			self.number.html(self.seconds - self.count);
+			self.count++;
+
+			if (self.count > (self.seconds - 1)) clearInterval(self.interval);
+
+			self.degrees = self.degrees + (360 / self.seconds);
+			if (self.count >= (self.seconds / 2)) {
+				self.slice.addClass("nc");
+				if (!self.slice.hasClass("mth")) self.pieRight.css({"transform":"rotate(180deg)"});
+				self.pieLeft.css({"transform":"rotate(" + self.degrees + "deg)"});
+				self.slice.addClass("mth");
+				if (self.count >= (self.seconds * 0.75)) self.quarter.remove();
+			} else {
+				self.pie.css({"transform":"rotate(" + self.degrees + "deg)"});
+			}
+		}, 1000);
+	}
+}
+
+var Timer;
+
+$(document).ready(function() {
+	Timer = new radialTimer();
+	Timer.init(20);
+});
