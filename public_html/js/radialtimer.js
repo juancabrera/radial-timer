4f88633f7b746da91b60fca9a5647cd5789428d7
Juan Cabrera <juan.acc@gmail.com>
2013-08-19 15:19:47 -0400

    injecting timer code on the javascript side

--- public_html/js/radialtimer.js
+++ public_html/js/radialtimer.js
@@ -5,14 +5,31 @@ function radialTimer() {
 	this.count = 0;
 	this.degrees = 0;
 	this.interval = null;
-	this.number = $(".n");
-	this.slice = $(".slice");
-	this.pie = $(".pie");
-	this.pieRight = $(".pie.r");
-	this.pieLeft = $(".pie.l");
-	this.quarter = $(".q");
+	this.timerHTML = "<div class='n'></div><div class='slice'><div class='q'></div><div class='pie r'></div><div class='pie l'></div></div>";
+	this.timerContainer = null;
+	this.number = null;
+	this.slice = null;
+	this.pie = null;
+	this.pieRight = null;
+	this.pieLeft = null;
+	this.quarter = null;
 
-	this.init = function(s) {
+	this.init = function(e, s) {
+		self.timerContainer = $("#" + e);
+		self.timerContainer.html(self.timerHTML);
+		
+		self.number = self.timerContainer.find(".n");
+		self.slice = self.timerContainer.find(".slice");
+		self.pie = self.timerContainer.find(".pie");
+		self.pieRight = self.timerContainer.find(".pie.r");
+		self.pieLeft = self.timerContainer.find(".pie.l");
+		self.quarter = self.timerContainer.find(".q");
+
+		// start timer
+		self.start(s);
+	}
+
+	this.start = function(s) {
 		self.seconds = s;
 		self.interval = window.setInterval(function () {
 			self.number.html(self.seconds - self.count);
@@ -38,5 +55,5 @@ var Timer;
 
 $(document).ready(function() {
 	Timer = new radialTimer();
-	Timer.init(20);
+	Timer.init("timer", 20);
 });
